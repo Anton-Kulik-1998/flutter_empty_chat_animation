28b548c83aa7bc40e7df1435fcc73f3bbf875b45
Антон Кулик <anton.smile.1998@gmail.com>
2024-09-11 14:59:05 +0300

    points_painter теперь отображает изображения

--- lib/src/ui/widgets/animated_background/points_painter.dart
+++ lib/src/ui/widgets/animated_background/points_painter.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 class PointsPainter extends CustomPainter {
@@ -5,7 +7,17 @@ class PointsPainter extends CustomPainter {
   final double maxDistance;
   final Color paintColor;
   final Color lineColor;
-  PointsPainter(this.points, this.maxDistance, this.paintColor, this.lineColor);
+  final ui.Image? image;
+  final double imageSize; // Добавляем размер изображения
+
+  PointsPainter(
+    this.points,
+    this.maxDistance,
+    this.paintColor,
+    this.lineColor,
+    this.image, {
+    this.imageSize = 50.0, // Размер изображения по умолчанию
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -14,19 +26,30 @@ class PointsPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 1.0;
 
-    // Draw points
-    for (final point in points) {
-      canvas.drawCircle(point, 50.0, paint);
+    if (image != null) {
+      // Рисуем изображение вместо точек с измененным размером
+      for (final point in points) {
+        // Исходные размеры изображения
+        final srcRect = Rect.fromLTWH(0, 0, image!.width.toDouble(), image!.height.toDouble());
+
+        // Прямоугольник, куда мы хотим поместить изображение (с центровкой)
+        final dstRect = Rect.fromCenter(
+          center: point,
+          width: imageSize,  // Ширина изображения
+          height: imageSize, // Высота изображения
+        );
+
+        // Рисуем изображение в новом размере
+        canvas.drawImageRect(image!, srcRect, dstRect, paint);
+      }
+    } else {
+      // Пока изображение не загружено, рисуем точки
+      for (final point in points) {
+        canvas.drawCircle(point, 50.0, paint);
+      }
     }
-
-
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
-
-
-// const Color.fromARGB(255, 0, 0, 0)
-
-//  Colors.black.withOpacity(1.0 - (distance / maxDistance));
