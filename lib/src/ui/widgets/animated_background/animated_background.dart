362d7fd519b33d71bc0efc21414a94f24b18ade6
Антон Кулик <anton.smile.1998@gmail.com>
2024-09-18 22:06:05 +0300

    Ренейм файла модели PointModel

--- lib/src/ui/widgets/animated_background/animated_background.dart
+++ lib/src/ui/widgets/animated_background/animated_background.dart
@@ -1,3 +1,3 @@
-export '../../../model/animated_background_model.dart';
+export '../../../model/point_model.dart';
 export '../../../view_model/animated_background/animated_background_view_model.dart';
 export 'animated_background_widget.dart';
